68b0ae9f0642173088138749e7734a3a9e93b209
Abhi <abhishekmrm1998@gmail.com>
2020-09-12 23:20:21 +0530

    simple color changes

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getInputValue() {
     var bmiRangeText = 'you are underweight';
     documentH3.innerHTML = innerText;
     documentH2.innerHTML = bmiRangeText;
-    documentH2.style.color = 'yellow';
+    documentH2.style.color = 'sandybrown';
   } else if (bmi > 18.5 && bmi <= 25) {
     var innerText = 'your body to mass ratio is ' + bmi;
     var bmiRangeText = 'you are normal';
@@ -22,10 +22,10 @@ function getInputValue() {
     documentH2.style.color = 'green';
   } else if (bmi > 25 && bmi <= 30) {
     var innerText = 'your body to mass ratio is ' + bmi;
-    var bmiRangeText = 'you are normal';
+    var bmiRangeText = 'you are overweight';
     documentH3.innerHTML = innerText;
     documentH2.innerHTML = bmiRangeText;
-    documentH2.style.color = 'yellow';
+    documentH2.style.color = 'sandybrown';
   } else if (bmi > 30) {
     var innerText = 'your body to mass ratio is ' + bmi;
     var bmiRangeText = 'you are obese';
